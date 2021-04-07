2312bdb0f4cf5706b3b231e442490aa7a2cc6aa9
FedericoBeeche <fbeeche@gmail.com>
2021-04-07 03:04:16 +0000

    empezando la base de datos, modelos listos, siguen funciones sqlalchemy

--- src/models.py
+++ src/models.py
@@ -4,16 +4,53 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120), nullable=False)
+    username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    population = db.Column(db.Integer)
+    capital = db.Column(db.String(250))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "capital": self.capital,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(250), nullable = False)
+    lastname = db.Column(db.String(250))
+    age = db.Column(db.Integer)
+    ship = db.Column(db.String(250))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age,
+            "ship": self.ship,
             # do not serialize the password, its a security breach
         }

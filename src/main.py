3a3090e08209abf47a161373a79ce226bc8df5b1
FedericoBeeche <fbeeche@gmail.com>
2021-04-08 03:04:56 +0000

    updates a funciones de get, post, put, delete en main.py

--- src/main.py
+++ src/main.py
@@ -31,13 +31,10 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+def getAllUsers():
+    all_users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), all_users))
+    return jsonify(all_users), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     username = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
 
     # def __repr__(self):
     #     return '<User %r>' % self.username

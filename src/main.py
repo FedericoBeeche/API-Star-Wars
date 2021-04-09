f00379284f321641acf20ba85b370577c8501421
FedericoBeeche <fbeeche@gmail.com>
2021-04-09 02:34:11 +0000

    todas las funcionoes de GET, PUT, POST, DELETE terminadas

--- src/main.py
+++ src/main.py
@@ -30,6 +30,8 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+####### USER FUNCTIONS #######
+
 @app.route('/user', methods=['GET'])
 def getAllUsers():
     all_users = User.query.all()
@@ -77,6 +79,104 @@ def deleteUsers(id):
     db.session.commit()
     return("The user has been deleted")
 
+####### USER FUNCTIONS #######
+
+####### PLANET FUNCTIONS #######
+
+@app.route('/planet', methods=['GET'])
+def getAllPlanets():
+    all_planets = Planet.query.all()
+    all_planets = list(map(lambda x: x.serialize(), all_planets))
+    return jsonify(all_planets), 200
+
+@app.route('/planet/<int:id>', methods=['GET'])
+def getSinglePlanet(id):
+    planet = Planet.query.get(id)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planet', methods=['POST'])
+def createPlanet():
+    body = request.get_json()
+    planet = Planet(name=body["name"], population=body["population"], capital=body["capital"])
+    db.session.add(planet)
+    db.session.commit()
+    print("Planet has been created!", body)
+    return jsonify(body), 200
+
+@app.route('/planet/<int:id>', methods=['PUT'])
+def updatePlanet(id):
+    body = request.get_json()
+    planetToUpdate = Planet.query.get(id)
+    if planetToUpdate is None:
+        raise APIException('User not found', status_code=404)
+
+    if "name" in body:
+        planetToUpdate.name = body["name"]
+    if "population" in body:
+        planetToUpdate.population = body["population"]
+    if "capital" in body:
+        planetToUpdate.capital = body["capital"]
+    db.session.commit()
+    return("Planet has been updated correctly!")
+
+@app.route('/planet/<int:id>', methods=['DELETE'])
+def deletePlanets(id):
+    planet = Planets.query.get(id)
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+    db.session.delete(planet)
+    db.session.commit()
+    return("The planet has been deleted")
+
+####### CHARACTER FUNCTIONS #######
+
+@app.route('/character', methods=['GET'])
+def getAllCharacters():
+    all_characters = Character.query.all()
+    all_characters = list(map(lambda x: x.serialize(), all_characters))
+    return jsonify(all_characters), 200
+
+@app.route('/character/<int:id>', methods=['GET'])
+def getSingleCharacter(id):
+    character = Character.query.get(id)
+    return jsonify(character.serialize()), 200
+
+@app.route('/character', methods=['POST'])
+def createCharacter():
+    body = request.get_json()
+    character = Character(name=body["name"], lastname=body["lastname"], age=body["age"], ship=body["ship"])
+    db.session.add(character)
+    db.session.commit()
+    print("Character has been created!", body)
+    return jsonify(body), 200
+
+@app.route('/character/<int:id>', methods=['PUT'])
+def updateCharacter(id):
+    body = request.get_json()
+    characterToUpdate = Planet.query.get(id)
+    if characterToUpdate is None:
+        raise APIException('Character not found', status_code=404)
+
+    if "name" in body:
+        characterToUpdate.name = body["name"]
+    if "lastname" in body:
+        characterToUpdate.lastname = body["lastname"]
+    if "age" in body:
+        characterToUpdate.age = body["age"]
+    if "ship" in body:
+        characterToUpdate.ship = body["ship"]
+    db.session.commit()
+    return("Character has been updated correctly!")
+
+@app.route('/character/<int:id>', methods=['DELETE'])
+def deleteCharacters(id):
+    character = Characters.query.get(id)
+    if character is None:
+        raise APIException('Character not found', status_code=404)
+    db.session.delete(character)
+    db.session.commit()
+    return("The character has been deleted")
+    
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))

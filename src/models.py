3a40efb027f764f29aa0a8a8c7960cdd6fa57780
FedericoBeeche <fbeeche@gmail.com>
2021-04-12 03:49:19 +0000

    Update: back terminado

--- src/models.py
+++ src/models.py
@@ -11,6 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    favorites = db.relationship('Favorites', lazy=True)
 
     # def __repr__(self):
     #     return '<User %r>' % self.username
@@ -30,7 +31,7 @@ class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     category = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def serialize(self):
         return {
